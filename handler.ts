72b69459cb82f8e9895ef6dec652029eaee063f1
Odavinh <penxerscky@gmail.com>
2020-03-26 17:21:42 +0200

    add graphql defaul server

--- handler.ts
+++ handler.ts
@@ -1,10 +1,18 @@
-import awsExpress from 'aws-serverless-express';
+import { ApolloServer } from 'apollo-server-lambda';
 import 'source-map-support/register';
 
-import app from './src/app';
+import { resolvers } from './src/resolvers';
+import { typeDefs } from './src/typeDefs';
 
-const server = awsExpress.createServer(app);
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ event, context }) => ({
+    headers: event.headers,
+    functionName: context.functionName,
+    event,
+    context,
+  }),
+});
 
-export const lambda = (event, context) => {
-  awsExpress.proxy(server, event, context);
-};
+export const handler = server.createHandler();
